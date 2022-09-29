6c3f5d3ed9bb91acc1a43e13a443752eca1c5f7f
Peyton C <peyton.castill0@icloud.com>
2022-09-29 14:27:44 -0400

    Added update a category by its `id`.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,22 @@ router.post('/', async (req, res) => {
 }
 );
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const UpdateCat = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!UpdateCat[0]) {
+      res.status(404).json({ message: '404 No category with this id.' });
+      return;
+    }
+    res.status(200).json({ message: 'Category successfuly updated from database.' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
