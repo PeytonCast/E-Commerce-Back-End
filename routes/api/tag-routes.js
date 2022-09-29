4e0292b158c4d7aa45231bc29c600620215f6227
Peyton C <peyton.castill0@icloud.com>
2022-09-29 17:09:54 -0400

    added create a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
- 
   try {
     const allTag = await Tag.findAll({ include : [{model: Product}]});
     res.status(200).json(allTag);
@@ -16,7 +15,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, { include : [{model: Product}]}
     );
@@ -30,8 +28,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(201).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 router.put('/:id', (req, res) => {
